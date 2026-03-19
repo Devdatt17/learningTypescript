54b5aacec2534fd9e86bec6f187951a3b9f7a07d
Devdatt17 <devdattpandit99@gmail.com>
2026-03-19 20:23:30 +0530

    understood handling optional parameters

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 function morningGreet(person, date) {
     return "Hello ".concat(person, " Today is ").concat(date.toDateString());
 }
+// return type anotations
+function getAnyNumber() {
+    return 14;
+}
+// optional properties -> use ?:
+function printName(firstName, lastName, middleName) {
+    if (middleName) {
+        return "Welcome, ".concat(firstName, " ").concat(middleName, " ").concat(lastName);
+    }
+    else {
+        return "Hello, ".concat(firstName, " ").concat(lastName);
+    }
+}
+// function that accepts only certain set of known values
+function printText(s, name) {
+    return "".concat(s, " for ").concat(name);
+}
 morningGreet("Dev", new Date());
+getAnyNumber();
+console.log(printName("Devdatt", "Pandit", "Ganesh"));
+printText("Pancake", "Dev");
--- index.ts
+++ index.ts
@@ -2,4 +2,26 @@ function morningGreet(person: string, date: Date) {
     return `Hello ${person} Today is ${date.toDateString()}`
 }
 
+// return type anotations
+function getAnyNumber() : number {
+    return 14
+}
+
+// optional properties -> use ?:
+function printName(firstName: string, lastName: string, middleName?: string) {
+    if(middleName) {
+        return `Welcome, ${firstName} ${middleName} ${lastName}`
+    } else {
+    return `Hello, ${firstName} ${lastName}`
+    }
+}
+
+// function that accepts only certain set of known values
+function printText(s: string, name: "Dev" | "Pan" | "GST") {
+    return `${s} for ${name}`
+}
+
 morningGreet("Dev", new Date())
+getAnyNumber();
+console.log(printName("Devdatt", "Pandit", "Ganesh"))
+printText("Pancake", "Dev")
